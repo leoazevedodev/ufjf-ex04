7bbfeeac161244ca35e52eaaa6c3a821121739c8
Leo Azevedo <leozinnnn3@gmail.com>
2023-04-15 17:31:13 -0300

    first commit

---
+++ Aluno.cpp
@@ -0,0 +1,58 @@
+#include "Aluno.h"
+
+Aluno::Aluno(string n, string m)
+{
+    cout << "Criando aluno" << endl;
+    nome = n;
+    matricula = m;
+}
+
+Aluno::~Aluno()
+{
+    cout << "Destruindo objeto aluno" << endl;
+}
+
+// ----------------------------------------------------------------------------
+// Exercicio 1
+// ----------------------------------------------------------------------------
+
+void Aluno::leNotas()
+{
+    for(int i = 0; i < 7; i++)
+    {
+        cout << "Digite a nota " << i + 1 << " : ";
+        cin >> notas[i];
+    }
+}
+
+double Aluno::calculaMedia()
+{
+    // Implemente sua solucao aqui
+    return 0;
+}
+
+// ----------------------------------------------------------------------------
+
+void Aluno::setNome(string novo)
+{
+    nome = novo;
+}
+
+string Aluno::getNome()
+{
+    return nome;
+}
+
+// ----------------------------------------------------------------------------
+// Exercicio 2
+// ----------------------------------------------------------------------------
+
+// Implemente aqui a sua solucao
+
+
+// ----------------------------------------------------------------------------
+// Exercicio 3
+// ----------------------------------------------------------------------------
+
+// Implemente aqui sua solucao
+
